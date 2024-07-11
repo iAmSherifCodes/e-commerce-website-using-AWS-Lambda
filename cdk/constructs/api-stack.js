e2bcc7a9e3b4f6b5a043332957250f042487ff54
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-11 10:31:51 +0100

    Created a CdnParameter for retrieve a string value from ssmParameter store

--- cdk/constructs/api-stack.js
+++ cdk/constructs/api-stack.js
@@ -1,4 +1,4 @@
-const { Stack, Fn, CfnOutput } = require('aws-cdk-lib')
+const { Stack, Fn, CfnOutput, CfnParameter } = require('aws-cdk-lib')
 const { Runtime } = require('aws-cdk-lib/aws-lambda')
 const { RestApi, LambdaIntegration, AuthorizationType, CfnAuthorizer } = require('aws-cdk-lib/aws-apigateway')
 const { NodejsFunction } = require('aws-cdk-lib/aws-lambda-nodejs')
@@ -8,6 +8,11 @@ const { StringParameter } = require('aws-cdk-lib/aws-ssm')
 class ApiStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props)
+
+    new CfnParameter(this, "KmsArnParameter", {
+      type: "AWS::SSM::Parameter::Value<String>",
+      default: `/${props.serviceName}/${props.ssmStageName}/kmsArn`
+    })
 
     const api = new RestApi(this, `${props.stageName}-MyApi`, {
       deployOptions: {
