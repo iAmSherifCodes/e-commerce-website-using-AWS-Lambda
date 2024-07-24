e58a2d2a7cab3c0adaa905c4f7cff281732fb6ec
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-24 14:55:08 +0100

    Configured event bus to main.js file

--- cdk/main.js
+++ cdk/main.js
@@ -4,10 +4,12 @@ const cdk = require('aws-cdk-lib')
 const { ApiStack } = require('./constructs/api-stack')
 const { DatabaseStack } = require('./constructs/database-stack')
 const { CognitoStack } = require('./constructs/cognito-stack')
+const {EventStack} = require("./constructs/events-stack");
 
 const app = new cdk.App()
 let stageName = app.node.tryGetContext('stageName')
 let ssmStageName = app.node.tryGetContext('ssmStageName')
+const serviceName= 'workshop-sherif'
 
 if (!stageName) {
   console.log('Defaulting stage name to dev')
@@ -21,12 +23,13 @@ if (!ssmStageName) {
 
 const dbStack = new DatabaseStack(app, `DatabaseStack-${stageName}`, { stageName })
 const cognitoStack = new CognitoStack(app, `CognitoStack-${stageName}`, { stageName })
+const eventsStack = new EventStack(app, `EventStack-${stageName}`, {serviceName ,stageName, ssmStageName})
 new ApiStack(app, `ApiStack-${stageName}`, { 
   stageName,
   ssmStageName,
-  serviceName: 'workshop-sherif',
+  serviceName,
   restaurantsTable: dbStack.restaurantsTable,
   cognitoUserPool: cognitoStack.cognitoUserPool,
   webUserPoolClient: cognitoStack.webUserPoolClient,
-  serverUserPoolClient: cognitoStack.serverUserPoolClient
+  serverUserPoolClient: cognitoStack.serverUserPoolClient,
 })
