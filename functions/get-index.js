f55b5994315a119990920606f2432e721b9c41a6
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-24 16:12:48 +0100

    Added placeOrder api to index-function

--- functions/get-index.js
+++ functions/get-index.js
@@ -8,6 +8,7 @@ const restaurantsApiRoot = process.env.restaurants_api;
 const cognitoUserPoolId = process.env.cognito_user_pool_id;
 const cognitoClientId = process.env.cognito_client_id;
 const awsRegion = process.env.AWS_REGION;
+const ordersApiRoot = process.env.orders_api;
 
 const days = [
   "Sunday",
@@ -48,6 +49,7 @@ module.exports.handler = async (event, context) => {
     dayOfWeek,
     restaurants,
     searchUrl: `${restaurantsApiRoot}/search`,
+    placeOrderUrl: ordersApiRoot
   };
   const html = Mustache.render(template, view);
   const response = {
