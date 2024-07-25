d0140306319c1081f21e436391f4eb709f06dae9
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-25 16:17:25 +0100

    Configuring SQS in EventsStack for End-2-End testing

--- cdk/constructs/events-stack.js
+++ cdk/constructs/events-stack.js
@@ -1,8 +1,10 @@
-const { Stack } = require('aws-cdk-lib')
+const { Stack, CfnCondition, Duration, Fn } = require('aws-cdk-lib')
 const { EventBus, Rule } = require('aws-cdk-lib/aws-events')
 const { LambdaFunction } = require('aws-cdk-lib/aws-events-targets')
-const { Topic } = require('aws-cdk-lib/aws-sns')
+const { Topic, Subscription } = require('aws-cdk-lib/aws-sns')
 const { Function, Code, Runtime } = require('aws-cdk-lib/aws-lambda')
+const { Queue } = require('aws-cdk-lib/aws-sqs')
+const { PolicyStatement, ServicePrincipal } = require('aws-cdk-lib/aws-iam')
 
 class EventsStack extends Stack {
     constructor(scope, id, props) {
@@ -36,6 +38,37 @@ class EventsStack extends Stack {
             }
         })
         rule.addTarget(new LambdaFunction(notifyRestaurantFunction))
+
+        const isE2eTest = props.stageName.startsWith('dev')
+        const condition = new CfnCondition(this, 'IsE2eTest', {
+            expression: Fn.conditionEquals('true', isE2eTest)
+        })
+
+        const testQueue = new Queue(this, 'E2eTestQueue', {
+            retentionPeriod: Duration.seconds(60),
+            visibilityTimeout: Duration.seconds(1)
+        })
+        testQueue.node.defaultChild.cfnOptions.condition = condition
+        if (isE2eTest) {
+            testQueue.addToResourcePolicy(new PolicyStatement({
+                actions: ['sqs:SendMessage'],
+                resources: [testQueue.queueArn],
+                principals: [new ServicePrincipal('sns.amazonaws.com')],
+                conditions: {
+                    ArnEquals: {
+                        'aws:SourceArn': restaurantNotificationTopic.topicArn,
+                    },
+                }
+            }))
+        }
+
+        const subscription = new Subscription(this, 'E2eTestSnsSubscription', {
+            topic: restaurantNotificationTopic,
+            protocol: 'sqs',
+            endpoint: testQueue.queueArn,
+            rawMessageDelivery: false
+        })
+        subscription.node.defaultChild.cfnOptions.condition = condition
     }
 }
 
