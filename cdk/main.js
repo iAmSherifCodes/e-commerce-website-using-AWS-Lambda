d7a52f7e663b638b6a23aecb177b9a5d87a2708d
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-10-11 12:08:16 +0100

    Initialized OrderFlow Step-functions in main.js file

--- cdk/main.js
+++ cdk/main.js
@@ -4,12 +4,12 @@ const cdk = require('aws-cdk-lib')
 const { ApiStack } = require('./constructs/api-stack')
 const { DatabaseStack } = require('./constructs/database-stack')
 const { CognitoStack } = require('./constructs/cognito-stack')
-const {EventsStack} = require("./constructs/events-stack");
+const { EventsStack } = require('./constructs/events-stack')
+const { OrderFlowStack } = require('./constructs/order-flow-stack')
 
 const app = new cdk.App()
 let stageName = app.node.tryGetContext('stageName')
 let ssmStageName = app.node.tryGetContext('ssmStageName')
-const serviceName= 'workshop-sherif'
 
 if (!stageName) {
   console.log('Defaulting stage name to dev')
@@ -21,21 +21,31 @@ if (!ssmStageName) {
   ssmStageName = stageName
 }
 
+const serviceName = 'workshop-sherif'
+
 const dbStack = new DatabaseStack(app, `DatabaseStack-${stageName}`, { stageName })
 const cognitoStack = new CognitoStack(app, `CognitoStack-${stageName}`, { stageName })
-const eventsStack = new EventsStack(app, `EventStack-${stageName}`, {
+const eventsStack = new EventsStack(app, `EventsStack-${stageName}`, {
   serviceName,
   stageName,
   ssmStageName,
-  idempotencyTable: dbStack.idempotencyTable,
+  idempotencyTable: dbStack.idempotencyTable
 })
-new ApiStack(app, `ApiStack-${stageName}`, { 
+new ApiStack(app, `ApiStack-${stageName}`, {
+  serviceName,
   stageName,
   ssmStageName,
-  serviceName,
   restaurantsTable: dbStack.restaurantsTable,
   cognitoUserPool: cognitoStack.cognitoUserPool,
   webUserPoolClient: cognitoStack.webUserPoolClient,
   serverUserPoolClient: cognitoStack.serverUserPoolClient,
   orderEventBus: eventsStack.orderEventBus
+})
+new OrderFlowStack(app, `OrderFlowStack-${stageName}`, {
+  serviceName,
+  stageName,
+  ordersTable: dbStack.ordersTable,
+  orderEventBus: eventsStack.orderEventBus,
+  restaurantNotificationTopic: eventsStack.restaurantNotificationTopic,
+  userNotificationTopic: eventsStack.userNotificationTopic
 })
